edff1ceabe32b53dcdbb4a90e541dc3abaf2770c
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-06-27 14:54:17 +0500

    add some services

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -8,18 +8,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CertificateContentComponent } from './contents/certificate-content/certificate-content.component';
 import { SideBarComponent } from './main-page/side-bar/side-bar.component';
 import { ContentHeaderComponent } from './main-page/content-header/content-header.component';
-import { LanguageContentComponent } from './contents/language-content/language-content.component';
+import { ContentBodyComponent } from './main-page/content-body/content-body.component';
 
 @NgModule({
-  declarations: [MainComponent,CertificateContentComponent, SideBarComponent, ContentHeaderComponent],
+  declarations: [MainComponent,CertificateContentComponent, SideBarComponent, ContentHeaderComponent, ContentBodyComponent],
   imports: [
     CommonModule,
     MatIconModule,
     DashboardRoutingModule,
-    // ReactiveFormsModule,
-    FormsModule,
-    LanguageContentComponent
+    FormsModule
   ],
-  exports: [MainComponent, CertificateContentComponent, SideBarComponent, ContentHeaderComponent]
+  exports: [MainComponent, CertificateContentComponent, SideBarComponent, ContentHeaderComponent, ContentBodyComponent]
 })
 export class DashboardModule { }
---
+++ src/app/modules/dashboard/main-page/content-body/content-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-body',
+  templateUrl: './content-body.component.html',
+  styleUrl: './content-body.component.scss'
+})
+export class ContentBodyComponent {
+
+}
